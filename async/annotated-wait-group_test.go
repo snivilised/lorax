f75a0aaf9defe759588d70d86b21e39753853de7
plastikfan <plastikfan@outlook.com>
2023-08-25 11:59:26 +0100

    feat(async): add annotated wait group (#35)

---
+++ async/annotated-wait-group_test.go
@@ -0,0 +1,42 @@
+package async_test
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+
+	"github.com/snivilised/lorax/async"
+)
+
+var _ = Describe("AnnotatedWaitGroup", func() {
+
+	Context("Add", func() {
+		It("should: add", func() {
+			var wg async.WaitGroupEx = &async.AnnotatedWaitGroup{}
+
+			wg.Add(1, "producer")
+		})
+	})
+
+	Context("Done", func() {
+		It("should: quit", func() {
+			var wg async.WaitGroupEx = &async.AnnotatedWaitGroup{}
+
+			wg.Add(1, "producer")
+			wg.Done("producer")
+		})
+	})
+
+	Context("Wait", func() {
+		It("should: quit", func() {
+			var wg async.WaitGroupEx = &async.AnnotatedWaitGroup{}
+
+			wg.Add(1, "producer")
+			go func() {
+				wg.Done("producer")
+			}()
+			<-time.After(time.Second / 10)
+			wg.Wait("main")
+		})
+	})
+})
