4c45791e2197d9c9efa6ec779a0cb70ea0f6e5b8
plastikfan <plastikfan@outlook.com>
2024-01-05 12:22:44 +0000

    ref(boost): improve structured logging (#109)

--- boost/worker.go
+++ boost/worker.go
@@ -25,28 +25,30 @@ func (w *worker[I, O]) run(parentContext context.Context,
 		id: w.id,
 	}
 	defer func(r *workerFinishedResult) {
-		w.finishedChOut <- r // ⚠️ non-pre-emptive send, but this should be ok
+		w.finishedChOut <- r // 💫 non-pre-emptive send, but this should be ok
 
-		w.logger.Debug(fmt.Sprintf("	<--- 🚀 worker.run(%v) (SENT FINISHED - error:'%v'). 🚀🚀🚀",
-			w.id, r.err,
-		))
+		w.logger.Debug("send complete",
+			slog.String("source", "worker.run"),
+			slog.String("worker-id", string(w.id)),
+		)
 	}(&result)
-
-	w.logger.Debug(fmt.Sprintf("	---> 🚀 worker.run(%v) ...(ctx:%+v)\n", w.id, parentContext))
 
 	for running := true; running; {
 		select {
 		case <-parentContext.Done():
-			w.logger.Debug(fmt.Sprintf(
-				"	---> 🚀 worker.run(%v)(finished) - done received 🔶🔶🔶", w.id,
-			))
+			w.logger.Debug("finished - done received",
+				slog.String("source", "worker.run"),
+				slog.String("worker-id", string(w.id)),
+			)
 
 			running = false
 		case job, ok := <-w.jobsChIn:
 			if ok {
-				w.logger.Debug(fmt.Sprintf(
-					"	---> 🚀 worker.run(%v)(input:'%v')", w.id, job.Input,
-				))
+				w.logger.Debug("read from channel",
+					slog.String("source", "worker.run"),
+					slog.String("worker-id", string(w.id)),
+					slog.String("job-input", fmt.Sprintf("%v", w.id)),
+				)
 
 				err := w.invoke(parentContext, parentCancel, outputChTimeout, job)
 
@@ -55,9 +57,10 @@ func (w *worker[I, O]) run(parentContext context.Context,
 					running = false
 				}
 			} else {
-				w.logger.Debug(fmt.Sprintf(
-					"	---> 🚀 worker.run(%v)(jobs chan closed) 🟥🟥🟥", w.id,
-				))
+				w.logger.Debug("jobs chan closed",
+					slog.String("source", "worker.run"),
+					slog.String("worker-id", string(w.id)),
+				)
 
 				running = false
 			}
@@ -78,22 +81,25 @@ func (w *worker[I, O]) invoke(parentContext context.Context,
 	result, _ := w.exec(job)
 
 	if w.outputsChOut != nil {
-		w.logger.Debug(fmt.Sprintf(
-			"	---> 🚀 worker.invoke ⏰ output timeout: '%v'", outputChTimeout,
-		))
+		w.logger.Debug("output timeout",
+			slog.String("source", "Worker.invoke"),
+			slog.String("output-channel-timeout", outputChTimeout.String()),
+		)
 
 		select {
 		case w.outputsChOut <- result:
 
 		case <-parentContext.Done():
-			w.logger.Debug(fmt.Sprintf(
-				"	---> 🚀 worker.invoke(%v)(cancel) - done received 💥💥💥", w.id,
-			))
+			w.logger.Debug("done received",
+				slog.String("source", "Worker.invoke"),
+				slog.String("worker-id", string(w.id)),
+			)
 
 		case <-outputContext.Done():
-			w.logger.Debug(fmt.Sprintf(
-				"	---> 🚀 worker.invoke(%v)(cancel) - timeout on send 👿👿👿", w.id,
-			))
+			w.logger.Debug("cancel - timeout on send",
+				slog.String("source", "Worker.invoke"),
+				slog.String("worker-id", string(w.id)),
+			)
 
 			// ??? err = i18n.NewOutputChTimeoutError()
 			err = errors.New("timeout on send")
