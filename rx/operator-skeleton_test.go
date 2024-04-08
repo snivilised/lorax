20363e20e93158bacf911349a9415aa4d1a9b454
plastikfan <plastikfan@outlook.com>
2024-04-08 11:12:33 +0100

    test(rx): add operator skeleton test (#149)

---
+++ rx/operator-skeleton_test.go
@@ -0,0 +1,51 @@
+package rx_test
+
+import (
+	"github.com/fortytw2/leaktest"
+	. "github.com/onsi/ginkgo/v2" //nolint:revive // ginkgo ok
+	. "github.com/onsi/gomega"    //nolint:revive // gomega ok
+
+	"github.com/snivilised/lorax/rx"
+)
+
+var _ = Describe("Observable operator", func() {
+	Context("${{OPERATOR-NAME}}", func() {
+		Context("principle", func() {
+			// success path
+			It("🧪 should: ", func() {
+				// rxgo: Test_
+				defer leaktest.Check(GinkgoT())()
+
+				Expect(1).To(Equal(1))
+				rx.Just("delete-me")
+			})
+		})
+
+		Context("Errors", func() {
+			Context("given: foo", func() {
+				It("🧪 should: ", func() {
+					// rxgo: Test_
+					defer leaktest.Check(GinkgoT())()
+				})
+			})
+		})
+
+		Context("Parallel", func() {
+			Context("given: foo", func() {
+				It("🧪 should: ", func() {
+					// rxgo: Test_
+					defer leaktest.Check(GinkgoT())()
+				})
+			})
+		})
+
+		Context("Parallel/Error", func() {
+			Context("given: foo", func() {
+				It("🧪 should: ", func() {
+					// rxgo: Test_
+					defer leaktest.Check(GinkgoT())()
+				})
+			})
+		})
+	})
+})
