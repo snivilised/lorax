d3f633717d7c55a9c985392fbbb7b17adfc7af42
plastikfan <plastikfan@outlook.com>
2024-04-14 18:30:52 +0100

    feat(rx): add toslice operator (#216)

---
+++ rx/observable-operator-toslice_test.go
@@ -0,0 +1,54 @@
+package rx_test
+
+import (
+	"context"
+
+	"github.com/fortytw2/leaktest"
+	. "github.com/onsi/ginkgo/v2" //nolint:revive // ginkgo ok
+	. "github.com/onsi/gomega"    //nolint:revive // gomega ok
+	"github.com/snivilised/lorax/rx"
+)
+
+var _ = Describe("Observable operator", func() {
+	Context("ToSlice", func() {
+		When("principle", func() {
+			It("🧪 should: ", func() {
+				// rxgo: Test_Observable_ToSlice
+				defer leaktest.Check(GinkgoT())()
+
+				ctx, cancel := context.WithCancel(context.Background())
+				defer cancel()
+
+				items, err := testObservable[int](ctx, 1, 2, 3).ToSlice(5)
+
+				Expect(items).To(ContainElements([]rx.Item[int]{
+					rx.Of(1),
+					rx.Of(2),
+					rx.Of(3),
+				}))
+				Expect(items).To(HaveCap(5))
+				Expect(err).To(Succeed())
+			})
+		})
+
+		Context("Errors", func() {
+			When("error", func() {
+				It("🧪 should: ", func() {
+					// rxgo: Test_Observable_ToSlice_Error
+					defer leaktest.Check(GinkgoT())()
+
+					ctx, cancel := context.WithCancel(context.Background())
+					defer cancel()
+
+					items, err := testObservable[int](ctx, 1, 2, errFoo, 3).ToSlice(0)
+
+					Expect(items).To(ContainElements([]rx.Item[int]{
+						rx.Of(1),
+						rx.Of(2),
+					}))
+					Expect(err).To(MatchError(errFoo))
+				})
+			})
+		})
+	})
+})
